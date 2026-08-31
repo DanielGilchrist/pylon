ad1bf85adefe45324e341dff8a0877362dbba0b7
Daniel Gilchrist <danniieelg@gmail.com>
2026-08-31 23:02:58 +0100

    Turn Reconciliation into a record

--- src/pylon/core/reconciliation.cr
+++ src/pylon/core/reconciliation.cr
@@ -1,19 +1,8 @@
 module Pylon::Core
-  struct Reconciliation
-    getter base_changes : Array(Change)
-    getter local_changes : Array(Change)
-    getter remote_changes : Array(Change)
-    getter conflicts : Array(Conflict)
-    getter troubles : Array(Trouble)
-
-    def initialize(
-      @base_changes : Array(Change),
-      @local_changes : Array(Change),
-      @remote_changes : Array(Change),
-      @conflicts : Array(Conflict),
-      @troubles : Array(Trouble) = [] of Trouble,
-    )
-    end
-
-  end
+  record Reconciliation,
+    base_changes : Array(Change),
+    local_changes : Array(Change),
+    remote_changes : Array(Change),
+    conflicts : Array(Conflict),
+    troubles : Array(Trouble) = [] of Trouble
 end
